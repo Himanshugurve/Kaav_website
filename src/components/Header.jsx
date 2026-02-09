5a059094d16bfc04a4ea5b94ccce7fcfaea42859
Himanshu Gurve <himangurve@gmail.com>
2026-02-09 15:30:22 +0530

    image add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/* Logo (inline with content, slightly larger but constrained to header height) */}
         <a href="#hero" className="flex h-full items-center cursor-pointer">
           <img
-            src="/Images/Kaavv.png"
+            src="/Images/kaavv.png"
             alt="Kavv Logos"
             className="h-12 w-auto max-h-full object-contain transform scale-110 hover:scale-125 transition-transform duration-200"
           />
